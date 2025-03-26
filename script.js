ccb2ccb5cb76d01a9ab37997d058411fac996ff6
Arman Khosravi <makh.khosravi@gmail.com>
2025-03-26 16:53:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let currentQuestionIndex = 0;
+    let questions = [];
 
+    function loadQuestions() {
+        fetch("/questions")
+            .then(response => response.json())
+            .then(data => {
+                questions = data;
+                displayQuestion();
+            });
+    }
+
+    function displayQuestion() {
+        if (currentQuestionIndex < questions.length) {
+            document.getElementById("question").innerText = questions[currentQuestionIndex].question;
+        } else {
+            document.getElementById("question").innerText = "You have completed all questions!";
+        }
+    }
+
+    document.getElementById("runQuery").addEventListener("click", function () {
+        const query = document.getElementById("sqlQuery").value;
+        fetch("/run_query", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ query, index: currentQuestionIndex })
+        })
+            .then(response => response.json())
+            .then(data => {
+                document.getElementById("result").innerText = data.message;
+                if (data.correct) {
+                    currentQuestionIndex++;
+                    displayQuestion();
+                }
+            });
+    });
+
+    loadQuestions();
+});
